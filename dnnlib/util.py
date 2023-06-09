2db779eb602cecff5042669c4ae480996c7273a0
Anish Acharya <anishacharya@utexas.edu>
2023-06-09 13:04:24 -0500

    +

--- dnnlib/util.py
+++ dnnlib/util.py
@@ -30,7 +30,7 @@ import uuid
 
 from distutils.util import strtobool
 from typing import Any, List, Tuple, Union, Optional
-import s3fs
+# import s3fs
 from torch_utils import distributed as dist
 import torch
 import PIL
@@ -401,131 +401,127 @@ def create_dir(directory):
     if not directory.startswith('s3://'):
         os.makedirs(directory, exist_ok=True)
 
-def list_dir(directory):
-    if not directory.startswith('s3://'):
-        return os.listdir(directory)
-    else:
-        s3 = s3fs.S3FileSystem(anon=False)
-        return [x['Key'] for x in s3.listdir(directory)]        
-
-def is_dir(directory):
-    if not directory.startswith('s3://'):
-        return os.isdir(directory)
-    else:
-        s3 = s3fs.S3FileSystem(anon=False)
-        return s3.isdir(directory)       
+# def list_dir(directory):
+#     if not directory.startswith('s3://'):
+#         return os.listdir(directory)
+#     else:
+#         s3 = s3fs.S3FileSystem(anon=False)
+#         return [x['Key'] for x in s3.listdir(directory)]
+#
+# def is_dir(directory):
+#     if not directory.startswith('s3://'):
+#         return os.isdir(directory)
+#     else:
+#         s3 = s3fs.S3FileSystem(anon=False)
+#         return s3.isdir(directory)
     
-def is_file(filename):
-    if not filename.startswith('s3://'):
-        return os.path.isfile(filename)
-    else:
-        s3 = s3fs.S3FileSystem(anon=False)
-        return s3.isfile(filename)
+#
+
 
 
     
-def open_url(url: str, cache_dir: str = None, num_attempts: int = 10, verbose: bool = True, return_filename: bool = False, cache: bool = True, read_mode: str = 'rb') -> Any:
-    """Download the given URL and return a binary-mode file object to access the data."""
-    assert num_attempts >= 1
-    assert not (return_filename and (not cache))
-
-    # check it is an s3 path    
-    if url.startswith('s3://'):
-        s3 = s3fs.S3FileSystem(anon=False)
-        return s3.open(url, read_mode)
-
-    # Doesn't look like an URL scheme so interpret it as a local filename.
-    if not re.match('^[a-z]+://', url):
-        return url if return_filename else open(url, read_mode)
-
-    # Handle file URLs.  This code handles unusual file:// patterns that
-    # arise on Windows:
-    #
-    # file:///c:/foo.txt
-    #
-    # which would translate to a local '/c:/foo.txt' filename that's
-    # invalid.  Drop the forward slash for such pathnames.
-    #
-    # If you touch this code path, you should test it on both Linux and
-    # Windows.
-    #
-    # Some internet resources suggest using urllib.request.url2pathname() but
-    # but that converts forward slashes to backslashes and this causes
-    # its own set of problems.
-    if url.startswith('file://'):
-        filename = urllib.parse.urlparse(url).path
-        if re.match(r'^/[a-zA-Z]:', filename):
-            filename = filename[1:]
-        return filename if return_filename else open(filename, read_mode)
-
-    assert is_url(url)
-
-    # Lookup from cache.
-    if cache_dir is None:
-        cache_dir = make_cache_dir_path('downloads')
-
-    url_md5 = hashlib.md5(url.encode("utf-8")).hexdigest()
-    if cache:
-        cache_files = glob.glob(os.path.join(cache_dir, url_md5 + "_*"))
-        if len(cache_files) == 1:
-            filename = cache_files[0]
-            return filename if return_filename else open(filename, read_mode)
-
-    # Download.
-    url_name = None
-    url_data = None
-    with requests.Session() as session:
-        if verbose:
-            print("Downloading %s ..." % url, end="", flush=True)
-        for attempts_left in reversed(range(num_attempts)):
-            try:
-                with session.get(url) as res:
-                    res.raise_for_status()
-                    if len(res.content) == 0:
-                        raise IOError("No data received")
-
-                    if len(res.content) < 8192:
-                        content_str = res.content.decode("utf-8")
-                        if "download_warning" in res.headers.get("Set-Cookie", ""):
-                            links = [html.unescape(link) for link in content_str.split('"') if "export=download" in link]
-                            if len(links) == 1:
-                                url = requests.compat.urljoin(url, links[0])
-                                raise IOError("Google Drive virus checker nag")
-                        if "Google Drive - Quota exceeded" in content_str:
-                            raise IOError("Google Drive download quota exceeded -- please try again later")
-
-                    match = re.search(r'filename="([^"]*)"', res.headers.get("Content-Disposition", ""))
-                    url_name = match[1] if match else url
-                    url_data = res.content
-                    if verbose:
-                        print(" done")
-                    break
-            except KeyboardInterrupt:
-                raise
-            except:
-                if not attempts_left:
-                    if verbose:
-                        print(" failed")
-                    raise
-                if verbose:
-                    print(".", end="", flush=True)
-
-    # Save to cache.
-    if cache:
-        safe_name = re.sub(r"[^0-9a-zA-Z-._]", "_", url_name)
-        safe_name = safe_name[:min(len(safe_name), 128)]
-        cache_file = os.path.join(cache_dir, url_md5 + "_" + safe_name)
-        temp_file = os.path.join(cache_dir, "tmp_" + uuid.uuid4().hex + "_" + url_md5 + "_" + safe_name)
-        os.makedirs(cache_dir, exist_ok=True)
-        with open(temp_file, "wb") as f:
-            f.write(url_data)
-        os.replace(temp_file, cache_file) # atomic
-        if return_filename:
-            return cache_file
-
-    # Return data as file object.
-    assert not return_filename
-    return io.BytesIO(url_data)
+# def open_url(url: str, cache_dir: str = None, num_attempts: int = 10, verbose: bool = True, return_filename: bool = False, cache: bool = True, read_mode: str = 'rb') -> Any:
+#     """Download the given URL and return a binary-mode file object to access the data."""
+#     assert num_attempts >= 1
+#     assert not (return_filename and (not cache))
+#
+#     # check it is an s3 path
+#     if url.startswith('s3://'):
+#         s3 = s3fs.S3FileSystem(anon=False)
+#         return s3.open(url, read_mode)
+#
+#     # Doesn't look like an URL scheme so interpret it as a local filename.
+#     if not re.match('^[a-z]+://', url):
+#         return url if return_filename else open(url, read_mode)
+#
+#     # Handle file URLs.  This code handles unusual file:// patterns that
+#     # arise on Windows:
+#     #
+#     # file:///c:/foo.txt
+#     #
+#     # which would translate to a local '/c:/foo.txt' filename that's
+#     # invalid.  Drop the forward slash for such pathnames.
+#     #
+#     # If you touch this code path, you should test it on both Linux and
+#     # Windows.
+#     #
+#     # Some internet resources suggest using urllib.request.url2pathname() but
+#     # but that converts forward slashes to backslashes and this causes
+#     # its own set of problems.
+#     if url.startswith('file://'):
+#         filename = urllib.parse.urlparse(url).path
+#         if re.match(r'^/[a-zA-Z]:', filename):
+#             filename = filename[1:]
+#         return filename if return_filename else open(filename, read_mode)
+#
+#     assert is_url(url)
+#
+#     # Lookup from cache.
+#     if cache_dir is None:
+#         cache_dir = make_cache_dir_path('downloads')
+#
+#     url_md5 = hashlib.md5(url.encode("utf-8")).hexdigest()
+#     if cache:
+#         cache_files = glob.glob(os.path.join(cache_dir, url_md5 + "_*"))
+#         if len(cache_files) == 1:
+#             filename = cache_files[0]
+#             return filename if return_filename else open(filename, read_mode)
+#
+#     # Download.
+#     url_name = None
+#     url_data = None
+#     with requests.Session() as session:
+#         if verbose:
+#             print("Downloading %s ..." % url, end="", flush=True)
+#         for attempts_left in reversed(range(num_attempts)):
+#             try:
+#                 with session.get(url) as res:
+#                     res.raise_for_status()
+#                     if len(res.content) == 0:
+#                         raise IOError("No data received")
+#
+#                     if len(res.content) < 8192:
+#                         content_str = res.content.decode("utf-8")
+#                         if "download_warning" in res.headers.get("Set-Cookie", ""):
+#                             links = [html.unescape(link) for link in content_str.split('"') if "export=download" in link]
+#                             if len(links) == 1:
+#                                 url = requests.compat.urljoin(url, links[0])
+#                                 raise IOError("Google Drive virus checker nag")
+#                         if "Google Drive - Quota exceeded" in content_str:
+#                             raise IOError("Google Drive download quota exceeded -- please try again later")
+#
+#                     match = re.search(r'filename="([^"]*)"', res.headers.get("Content-Disposition", ""))
+#                     url_name = match[1] if match else url
+#                     url_data = res.content
+#                     if verbose:
+#                         print(" done")
+#                     break
+#             except KeyboardInterrupt:
+#                 raise
+#             except:
+#                 if not attempts_left:
+#                     if verbose:
+#                         print(" failed")
+#                     raise
+#                 if verbose:
+#                     print(".", end="", flush=True)
+#
+#     # Save to cache.
+#     if cache:
+#         safe_name = re.sub(r"[^0-9a-zA-Z-._]", "_", url_name)
+#         safe_name = safe_name[:min(len(safe_name), 128)]
+#         cache_file = os.path.join(cache_dir, url_md5 + "_" + safe_name)
+#         temp_file = os.path.join(cache_dir, "tmp_" + uuid.uuid4().hex + "_" + url_md5 + "_" + safe_name)
+#         os.makedirs(cache_dir, exist_ok=True)
+#         with open(temp_file, "wb") as f:
+#             f.write(url_data)
+#         os.replace(temp_file, cache_file) # atomic
+#         if return_filename:
+#             return cache_file
+#
+#     # Return data as file object.
+#     assert not return_filename
+#     return io.BytesIO(url_data)
 
 
 def print_tensor_stats(tensor, tensor_name):
